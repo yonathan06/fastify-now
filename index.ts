53b20dc353a59d0c33835de7f03d14789a7bc268
yonatan bendahan <yonatan.bendahan@gmail.com>
2020-06-04 16:13:53 +0300

    include all methods

--- index.ts
+++ index.ts
@@ -1,19 +1,29 @@
-import path from 'path';
-import fs from 'fs';
-import Http from 'http';
+import path from "path";
+import fs from "fs";
+import Http from "http";
 import fastify, {
   FastifyInstance,
   FastifyReply,
   FastifyRequest,
-} from 'fastify';
-import fp from 'fastify-plugin';
+} from "fastify";
+import fp from "fastify-plugin";
 
-const SupportedMethods = ['GET', 'POST', 'PUT', 'DELETE'];
+enum HTTPMethod {
+  GET = "get",
+  POST = "post",
+  PUT = "put",
+  DELETE = "delete",
+  OPTIONS = "options",
+  PATCH = "patch",
+  HEAD = "head",
+  CONNECT = "connect",
+  TRACE = "trace",
+}
 
 export function registerRoutes(
   server: FastifyInstance,
-  folder,
-  pathPrefix = ''
+  folder: string,
+  pathPrefix = ""
 ) {
   fs.readdirSync(folder, { withFileTypes: true }).forEach((folderOrFile) => {
     const currentPath = path.join(folder, folderOrFile.name);
@@ -21,18 +31,18 @@ export function registerRoutes(
     if (folderOrFile.isDirectory()) {
       registerRoutes(server, currentPath, routeServerPath);
     } else if (folderOrFile.isFile()) {
-      if (!folderOrFile.name.endsWith('.js')) {
+      if (!folderOrFile.name.endsWith(".js")) {
         return;
       }
       let fileRouteServerPath = pathPrefix;
-      if (folderOrFile.name !== 'index.js') {
-        fileRouteServerPath += '/' + folderOrFile.name.replace('.js', '');
+      if (folderOrFile.name !== "index.js") {
+        fileRouteServerPath += "/" + folderOrFile.name.replace(".js", "");
       }
       if (fileRouteServerPath.length === 0) {
-        fileRouteServerPath = '/';
+        fileRouteServerPath = "/";
       }
       let module = require(currentPath);
-      SupportedMethods.forEach((method) => {
+      Object.values(HTTPMethod).forEach((method) => {
         addModuleMethod(module, method, server, fileRouteServerPath);
       });
     }
@@ -41,33 +51,14 @@ export function registerRoutes(
 
 function addModuleMethod(
   module: any,
-  method: string,
+  method: HTTPMethod,
   server: fastify.FastifyInstance,
   fileRouteServerPath: string
 ) {
-  const handler = module[method] as fastify.NowRequestHandler;
+  const handler = module[method.toUpperCase()] as fastify.NowRequestHandler;
   if (handler) {
-    const methodFunctionName = method.toLowerCase();
     server.log.debug(`${method.toUpperCase()} ${fileRouteServerPath}`);
-    server[methodFunctionName](
-      fileRouteServerPath,
-      handler.opts || {},
-      (req: FastifyRequest, reply: FastifyReply<Http.ServerResponse>) => {
-        handler
-          .bind(server)(req, reply)
-          .then((response) => {
-            if (response) {
-              reply.send(response);
-            }
-          })
-          .catch((error) => {
-            server.log.error(error);
-            if (!reply.sent) {
-              reply.status(500).send({ message: 'Internal server error' });
-            }
-          });
-      }
-    );
+    server[method](fileRouteServerPath, handler.opts || {}, handler);
   }
 }
 
@@ -82,7 +73,7 @@ function fastifyNow(
   next?: (error?: any) => void
 ) {
   if (!(opts && opts.routesFolder)) {
-    next(new Error('fastify-now: must provide opts.routesFolder'));
+    next(new Error("fastify-now: must provide opts.routesFolder"));
     return;
   }
   try {
@@ -93,7 +84,7 @@ function fastifyNow(
   }
 }
 
-declare module 'fastify' {
+declare module "fastify" {
   interface NowRequestHandler extends fastify.RequestHandler {
     opts?: fastify.RouteShorthandOptions;
   }
--- test/regular-server/test-server/routes/user/index.ts
+++ test/regular-server/test-server/routes/user/index.ts
@@ -1,27 +1,27 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 export const POST: fastify.NowRequestHandler = async (req, rep) => {
   if (req.body.name === 'Jon Doe') {
-    /** 
+    /**
      * in async function, you can return undefined if you already sent a response
      * then it won't try to send a response again with the returned value;
      */
-    rep.status(400).send({ message: 'Name can not be Jon Doe' });
-    return;
+    rep.status(400);
+    return { message: 'Name can not be Jon Doe' };
   }
   return { ...req.body };
-} 
+};
 
 POST.opts = {
-  schema: { 
+  schema: {
     body: {
       type: 'object',
       properties: {
         name: {
-          type: 'string'
-        }
+          type: 'string',
+        },
       },
-      required: ['name']
-    }
-  }
-}
+      required: ['name'],
+    },
+  },
+};
