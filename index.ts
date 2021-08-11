cbde7f40b3e6977bcadecb89ecd9cc40e5c823f8
Miel Pandarou <miel+git@lly.fr>
2021-08-11 13:25:04 +0200

    accept .ts route files if supported

--- index.ts
+++ index.ts
@@ -27,6 +27,18 @@ enum HTTPMethod {
   TRACE = 'trace',
 }
 
+const typeScriptEnabled = Boolean(
+  process[Symbol.for('ts-node.register.instance')] || process.env.TS_NODE_DEV,
+);
+
+const extensions = [".js"];
+if (typeScriptEnabled) {
+  extensions.push(".ts");
+}
+
+const isRoute = (ext: string) => extensions.includes(ext);
+const isTest = (name: string) => name.endsWith(".test") || name.endsWith(".spec");
+
 function addRequestHandler(
   module: { [key in HTTPMethod]: NowRequestHandler },
   method: HTTPMethod,
@@ -47,12 +59,13 @@ export function registerRoutes(server: FastifyInstance, folder: string, pathPref
     if (folderOrFile.isDirectory()) {
       registerRoutes(server, currentPath, routeServerPath);
     } else if (folderOrFile.isFile()) {
-      if (!folderOrFile.name.endsWith('.js') || /\.(test)|(spec)\.js$/i.test(folderOrFile.name)) {
+      const { ext, name } = path.parse(folderOrFile.name);
+      if (!isRoute(ext) || isTest(name)) {
         return;
       }
       let fileRouteServerPath = pathPrefix;
-      if (folderOrFile.name !== 'index.js') {
-        fileRouteServerPath += '/' + folderOrFile.name.replace('[', ':').replace(/\]?.js/, '');
+      if (name !== 'index') {
+        fileRouteServerPath += '/' + name.replace('[', ':').replace(/\]?$/, '');
       }
       if (fileRouteServerPath.length === 0) {
         fileRouteServerPath = '/';
